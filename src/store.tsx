382c1161f3eb0b518ada0089c44bffaca8766d5d
Marcel Martino <marcelmartino2053@gmail.com>
2023-02-18 19:34:32 +0100

    Create msw server create loadRobots inside useApi cerate useApi Test

---
+++ src/store.tsx
@@ -0,0 +1,28 @@
+import { RobotsStructure } from "./types";
+
+export const robots: RobotsStructure = [
+  {
+    id: 1,
+    name: "patato",
+    imageUrl: "string;",
+    speed: 9,
+    endurance: 1,
+    creationDate: "dad",
+  },
+  {
+    id: 2,
+    name: "patato",
+    imageUrl: "string;",
+    speed: 1,
+    endurance: 1,
+    creationDate: "dad",
+  },
+  {
+    id: 3,
+    name: "patato",
+    imageUrl: "string;",
+    speed: 1,
+    endurance: 1,
+    creationDate: "dad",
+  },
+];
